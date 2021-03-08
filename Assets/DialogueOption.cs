255f46ad2ec17d4cd70f1b0125ffd0e9c545bdfc
Angie <digitalrep@live.com.au>
2021-03-08 18:04:41 +1100

    Fixed button listener argument issue

---
+++ Assets/DialogueOption.cs
@@ -0,0 +1,50 @@
+﻿
+public class DialogueOption 
+{
+    private int id;
+    private string button_text;
+    private string player_dialogue;
+    private string alison_dialogue;
+    private int option1_id;
+    private int option2_id;
+
+    public DialogueOption(int id, string button_text, string player_dialogue, string alison_dialogue, int option1, int option2)
+    {
+        this.id = id;
+        this.button_text = button_text;
+        this.player_dialogue = player_dialogue;
+        this.alison_dialogue = alison_dialogue;
+        this.option1_id = option1;
+        this.option2_id = option2;
+    }
+
+    public int get_id()
+    {
+        return this.id;
+    }
+
+    public string get_button_text()
+    {
+        return this.button_text;
+    }
+
+    public string get_player_dialogue()
+    {
+        return this.player_dialogue;
+    }
+
+    public string get_alison_dialogue()
+    {
+        return this.alison_dialogue;
+    }
+
+    public int get_option_1()
+    {
+        return this.option1_id;
+    }
+
+    public int get_option_2()
+    {
+        return this.option2_id;
+    }
+}
--- Assets/ScrollScript.cs
+++ Assets/ScrollScript.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#pragma strict
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,10 +22,23 @@ public class ScrollScript : MonoBehaviour
     public Button buttonOption1;
     public Button buttonOption2;
 
+    public DialogueOption chosenDialogue;
+
     GameObject scrollItemObj;
 
-    private int counter = 0;
+    private DialogueOption[] dialogueOptions = {
+        new DialogueOption(0, "", "", "", 1, 2),
+        new DialogueOption(1, "Curious Opening", "Hello, Alison. I found your profile intriguing... If a little... Mysterious.", "Hi! Thanks for messaging me. Yeah, I guess it is mysterious. Does that bother you?", 3, 4),
+        new DialogueOption(2, "Normal Opening", "Hey Alison, how are you?", "Hey there, I'm good. How are you?", 5, 6),
+        new DialogueOption(3, "Not Bothered", "No, not at all. A little mystery never hurt anyone.", "Well I'm glad to hear that, because I'm really not an open book.", 7, 8),
+        new DialogueOption(4, "Bothered", "Yeah, actually, it kinda does. It makes it hard to know what to talk to you about.", "Hmm. I see. Well, you could always ask me.", 9, 10),
+        new DialogueOption(5, "Lie", "Yeah, I'm great. What did you get up to today?", "Not much, to be honest. What about you?", 11, 12),
+        new DialogueOption(6, "Truth", "I'm okay, but... Actually... I've had a bad day. My boss just blamed me for something that wasn't my fault.", "Oh, no. That sucks. I'm sorry that happened.", 13, 14),
+    };
 
+    private int current_index = 0;
+
+    /*
     private string[] playerDialogues = {
         "Hello, Alison. I found your profile intriguing... If a little... Mysterious.",
         "Hey Alison, how are you?"
@@ -44,12 +59,14 @@ public class ScrollScript : MonoBehaviour
         "Well I'm glad to hear that, because I'm really not an open book.",
         "Me too. I always prefer watching the movie to reading the book."
     };
+    */
 
     private int suspicionCounter = 0;
 
     void Start()
     {
-
+        buttonOption1.onClick.AddListener(delegate { printDialogue(1); });
+        buttonOption2.onClick.AddListener(delegate { printDialogue(2); });
     }
 
     void Update()
@@ -60,10 +77,12 @@ public class ScrollScript : MonoBehaviour
     public void printDialogue(int option)
     {
         float beginTime = Time.time; 
-        int lineLength = 41;
+        int lineLength = 40;
 
-        int playerLength = playerDialogues[option].Length;
-        int alisonLength = alisonDialogues[option].Length;
+        DialogueOption chosenDialogue = dialogueOptions[option];
+
+        int playerLength = chosenDialogue.get_player_dialogue().Length;
+        int alisonLength = chosenDialogue.get_alison_dialogue().Length;
 
         int numLines = (int)Math.Ceiling((double)playerLength / lineLength);
 
@@ -71,18 +90,28 @@ public class ScrollScript : MonoBehaviour
         Debug.Log(playerLength / lineLength);
         Debug.Log((int)Math.Ceiling((double)playerLength / lineLength));
 
-        StartCoroutine(PrintPlayerDialogue(option, numLines));
+        StartCoroutine(PrintPlayerDialogue(option, numLines, chosenDialogue.get_player_dialogue()));
 
         StartCoroutine(ForceScrollDown());
 
         numLines = (int)Math.Ceiling((double)alisonLength / lineLength);
 
-        StartCoroutine(PrintAlisonDialogue(option, numLines));
+        StartCoroutine(PrintAlisonDialogue(option, numLines, chosenDialogue.get_alison_dialogue()));
 
-        //StartCoroutine(ForceScrollDown());
+        // Get next two options and fill buttons
+        current_index = option;
+
+        buttonOption1.onClick.RemoveAllListeners();
+        buttonOption2.onClick.RemoveAllListeners();
+
+        buttonOption1.GetComponentInChildren<Text>().text = dialogueOptions[chosenDialogue.get_option_1()].get_button_text();
+        buttonOption1.onClick.AddListener(delegate { printDialogue(dialogueOptions[chosenDialogue.get_option_1()].get_id()); });
+
+        buttonOption2.GetComponentInChildren<Text>().text = dialogueOptions[chosenDialogue.get_option_2()].get_button_text();
+        buttonOption1.onClick.AddListener(delegate { printDialogue(dialogueOptions[chosenDialogue.get_option_2()].get_id()); });
     }
 
-    IEnumerator PrintPlayerDialogue(int option, int numLines)
+    IEnumerator PrintPlayerDialogue(int option, int numLines, string dialogue)
     {
 
         if (numLines == 1)
@@ -90,7 +119,7 @@ public class ScrollScript : MonoBehaviour
             scrollItemObj = Instantiate(playerScrollItemPrefab);
             scrollItemObj.transform.SetParent(scrollContent.transform, false);
             scrollItemObj.transform.localScale = Vector3.one * 0.8f;
-            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = playerDialogues[option];
+            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = dialogue;
         }
 
         if (numLines == 2)
@@ -98,7 +127,7 @@ public class ScrollScript : MonoBehaviour
             scrollItemObj = Instantiate(playerScrollItemPrefab2);
             scrollItemObj.transform.SetParent(scrollContent.transform, false);
             scrollItemObj.transform.localScale = Vector3.one * 0.8f;
-            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = playerDialogues[option];
+            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = dialogue;
         }
 
         if (numLines == 3)
@@ -106,13 +135,13 @@ public class ScrollScript : MonoBehaviour
             scrollItemObj = Instantiate(playerScrollItemPrefab3);
             scrollItemObj.transform.SetParent(scrollContent.transform, false);
             scrollItemObj.transform.localScale = Vector3.one * 0.8f;
-            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = playerDialogues[option];
+            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = dialogue;
         }
 
         yield return null;
     }
 
-    IEnumerator PrintAlisonDialogue(int option, int numLines)
+    IEnumerator PrintAlisonDialogue(int option, int numLines, string dialogue)
     {
         yield return new WaitForSeconds(2);
 
@@ -121,7 +150,7 @@ public class ScrollScript : MonoBehaviour
             scrollItemObj = Instantiate(alisonScrollItemPrefab);
             scrollItemObj.transform.SetParent(scrollContent.transform, false);
             scrollItemObj.transform.localScale = Vector3.one * 0.8f;
-            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = alisonDialogues[option];
+            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = dialogue;
         }
 
         if (numLines == 2)
@@ -129,7 +158,7 @@ public class ScrollScript : MonoBehaviour
             scrollItemObj = Instantiate(alisonScrollItemPrefab2);
             scrollItemObj.transform.SetParent(scrollContent.transform, false);
             scrollItemObj.transform.localScale = Vector3.one * 0.8f;
-            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = alisonDialogues[option];
+            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = dialogue;
         }
 
         if (numLines == 3)
@@ -137,13 +166,12 @@ public class ScrollScript : MonoBehaviour
             scrollItemObj = Instantiate(alisonScrollItemPrefab3);
             scrollItemObj.transform.SetParent(scrollContent.transform, false);
             scrollItemObj.transform.localScale = Vector3.one * 0.8f;
-            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = alisonDialogues[option];
+            scrollItemObj.transform.Find("num").gameObject.GetComponent<Text>().text = dialogue;
         }
 
         StartCoroutine(ForceScrollDown());
 
     }
-
 
     IEnumerator ForceScrollDown()
     {
