19dc22052ed71ed35a9949b6392324d20f551404
angie <digitalrep@live.com.au>
2021-04-29 15:24:24 +1000

    Balloon looks deflated when you pop it now, just need it to fall faster

---
+++ Assets/Scripts/BalloonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BalloonScript : MonoBehaviour
+{
+    public Sprite deflated_balloon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision Enter detected");
+        gameObject.GetComponent<SpriteRenderer>().sprite = deflated_balloon;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger Enter detected");
+        Debug.Log(gameObject.GetComponentsInChildren<Image>().Length);  // returns 0
+        Debug.Log(transform.GetChild(2));
+        transform.GetChild(2).GetComponent<SpriteRenderer>().sprite = deflated_balloon;
+        gameObject.GetComponent<Rigidbody2D>().mass = 10;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/MoveBowAndArrow.cs
+++ Assets/Scripts/MoveBowAndArrow.cs
@@ -12,10 +12,12 @@ public class MoveBowAndArrow : MonoBehaviour
     float midWidth;
     float origX, origY;
     Vector3 origPosition;
+    
+    int currentArrow = 0;
+    int question = 1;
 
     GameObject[] quiver = new GameObject[6];
-    int currentArrow = 0;
-    GameObject balloon1;
+    GameObject[] balloons = new GameObject[10];
 
     bool fired = false;
 
@@ -33,7 +35,9 @@ public class MoveBowAndArrow : MonoBehaviour
         SpriteRenderer balloonImg = sprites[sprites.Length - 1];
         balloonImg.GetComponent<SpriteRenderer>().color = Color.green;
 
-        quiver[0] = Instantiate(arrow, new Vector3(-0.03f, -3.57f, 0), Quaternion.identity);
+        //gameObject.transform.rotation.eulerAngles.z
+        quiver[0] = Instantiate(arrow, new Vector3(-1.22f, -3.57f, 0), Quaternion.identity);
+        quiver[0].transform.Rotate(0, 0, -26.409f);
     }
 
     // Update is called once per frame
@@ -41,16 +45,17 @@ public class MoveBowAndArrow : MonoBehaviour
     {
         if(quiver[currentArrow].transform.position.y > 6f || quiver[currentArrow].transform.position.x > Screen.width)
         {
-            Debug.Log("Arrow out of bounds");
+            //Debug.Log("Arrow out of bounds");
             fired = false;
             Destroy(quiver[currentArrow]);
             currentArrow++;
             if(currentArrow < 5)
             {
-                quiver[currentArrow] = Instantiate(arrow, new Vector3(-0.03f, -3.57f, 0), Quaternion.identity);
-                gameObject.transform.rotation = new Quaternion(0, 0, -45f, 0);
-                gameObject.transform.position = new Vector3(-0.05f, -4.09f, 0);
-                //gameObject.transform.rotation = Quaternion.identity;
+                quiver[currentArrow] = Instantiate(arrow, new Vector3(-1.22f, -3.57f, 0), Quaternion.identity);
+                quiver[currentArrow].transform.Rotate(0, 0, -26.409f);
+                gameObject.transform.position = new Vector3(-1.34f, -3.98f, 0);
+                gameObject.transform.rotation = Quaternion.identity;
+                gameObject.transform.Rotate(0, 0, -26.909f);
             } else
             {
                 Debug.Log("Out of arrows");
@@ -59,31 +64,24 @@ public class MoveBowAndArrow : MonoBehaviour
 
         if(fired)
         {
-            /*
-            Vector3 arrowPos = quiver[currentArrow].transform.position;
-            Vector3 newPos = new Vector3(arrowPos.x + 0.01f, arrowPos.y + 0.1f, arrowPos.z);
-            quiver[currentArrow].transform.position = newPos;
-            */
-            //quiver[currentArrow].GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            quiver[currentArrow].GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10f);
-            //Debug.Log(quiver[currentArrow].GetComponentInChildren<Rigidbody2D>());
-            //quiver[currentArrow].GetComponent<Rigidbody2D>().velocity = quiver[currentArrow].GetComponentInChildren<Rigidbody2D>().GetRelativeVector(Vector2.up * 10f);
-            //quiver[currentArrow].GetComponent<Rigidbody2D>().velocity = quiver[currentArrow].GetComponent<Rigidbody2D>().GetRelativeVector(Vector2.up * 10f);
+            quiver[currentArrow].GetComponent<Rigidbody2D>().velocity = quiver[currentArrow].GetComponentInChildren<Rigidbody2D>().GetRelativeVector(Vector2.up * 10f);
         }
 
         if(Input.GetMouseButton(0))
         {
         
-            Debug.Log("Left mouse button pressed");
+            //Debug.Log("Left mouse button pressed");
 
-            if(Input.mousePosition.x != origPosition.x || Input.mousePosition.y != Input.mousePosition.y)
+            //quiver[currentArrow].GetComponent<Rigidbody2D>().transform.position = new Vector3(-1.37f, -3.50f, 0);
+
+            if (Input.mousePosition.x != origPosition.x || Input.mousePosition.y != Input.mousePosition.y)
             {
 
-                Debug.Log("Mouse Position Differs");
+                //Debug.Log("Mouse Position Differs");
 
                 if (Input.mousePosition.x < origPosition.x)
                 {
-                    Debug.Log("Mouse moved left");
+                    //Debug.Log("Mouse moved left");
 
                     BoxCollider2D collider = GetComponent<BoxCollider2D>();
                     Vector3 centre = collider.bounds.center;
@@ -93,14 +91,14 @@ public class MoveBowAndArrow : MonoBehaviour
 
                 if (Input.mousePosition.x > origPosition.x)
                 {
-                    Debug.Log("Mouse moved right");
+                    //Debug.Log("Mouse moved right");
 
                     BoxCollider2D collider = GetComponent<BoxCollider2D>();
                     Vector3 centre = collider.bounds.center;
                     transform.RotateAround(centre, Vector3.forward, -2);
                     quiver[currentArrow].transform.RotateAround(centre, Vector3.forward, -2);
 
-                    Debug.Log("Z euler angle of bow: " + gameObject.transform.rotation.eulerAngles.z);
+                    //Debug.Log("Z euler angle of bow: " + gameObject.transform.rotation.eulerAngles.z);
                 }
 
                 origPosition = Input.mousePosition;
@@ -110,7 +108,7 @@ public class MoveBowAndArrow : MonoBehaviour
 
         if(Input.GetMouseButtonUp(0))
         {
-            Debug.Log("Mouse button released");
+            //Debug.Log("Mouse button released");
             fired = true;
         }
 
